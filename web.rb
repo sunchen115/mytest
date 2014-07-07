b1bc30e36d250784b54b71df05014fa4813bd34e
sunchen115 <sunchen115work@gmail.com>
2014-07-07 16:44:19 +0800

    add interface

--- web.rb
+++ web.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
 
 get '/' do
-  "<h1> Hotel System</h1>"
+  "<h1> Hotel System</h1><br/>"+"<label>LiveInfo:</label><input type='text' id='liv_info'/><input type='button' id='search_cheapest' value='Search'></input>"
 end

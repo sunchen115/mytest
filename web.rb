bbf0d26962765e0313490dfd996355935e194441
sunchen115 <sunchen115work@gmail.com>
2014-07-08 13:12:40 +0800

    add search interface

--- web.rb
+++ web.rb
@@ -5,10 +5,10 @@ get '/posts' do
   htl_sys = HotelSystem.new
   htlinput = params[:query]  
 #htlinput = 'Regular: 20Mar2009(fri), 21Mar2009(sat), 22Mar2009(sun)'
-  puts htlinput
+  #puts htlinput
   cheapest_htl = htl_sys.find_cheapes_hotel(htlinput)
-  params.to_s
-#  haml :index,:locals => {:htl_input=>htlinput,:htl_output=>cheapest_htl} 
+  #params.to_s
+  haml :index,:locals => {:htl_input=>htlinput,:htl_output=>cheapest_htl} 
 end
 get '/hello' do
     'hello'
